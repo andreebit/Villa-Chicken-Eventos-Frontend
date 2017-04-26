afad7d92a1e6d47d72e07de728286b2e511942eb
Andreé Sandoval Rojas <asr@mambo.pe>
2017-04-26 15:33:29 -0500

    fix error that clear elements from package form

--- resources/views/packages/form.blade.php
+++ resources/views/packages/form.blade.php
@@ -2,6 +2,7 @@
 
 
 @section('content')
+    <?php $old = session()->getOldInput(); ?>
 
     <ol class="breadcrumb">
         <li><a href="{{ route('home.index') }}">Eventos</a></li>
@@ -35,47 +36,72 @@
 
     <div class="panel panel-default">
         <div class="panel-body">
-            <h3>Elementos</h3>
+            <h3>Incluye</h3>
             <table class="table table-hover" id="tbl_elements_package">
                 <thead>
                 <tr>
                     <th>Descripción</th>
                     <th>Categoría</th>
-                    <th><a href="#" type="button" class="btn btn-primary btn-sm" id="add_element_package">Agregar</a></th>
+                    <th><a href="#" type="button" class="btn btn-primary btn-sm" id="add_element_package">Agregar</a>
+                    </th>
                 </tr>
                 </thead>
                 <tbody>
 
-                @if(isset($package->items) && !empty($package->items))
-                    @foreach($package->items as $index => $item)
-                        <?php
+
+                @if(empty($old))
+                    @if(isset($package->items) && !empty($package->items))
+                        @foreach($package->items as $index => $item)
+                            <?php
                             $identifier = uniqid();
-                        ?>
+                            ?>
+                            <tr>
+                                <td>
+                                    {{ Form::text('description[' . $identifier . ']', $item->description, ['class' => 'form-control', 'placeholder' => 'Ingresa la descripción del elemento', 'maxlength' => 100, 'required' => 'required']) }}
+                                </td>
+                                <td>
+                                    {{ Form::select('service_category_id[' . $identifier . ']', $serviceCategories, $item->service_category->id, ['class' => 'form-control', 'placeholder' => 'Escoja una categoría', 'required' => 'required']) }}
+                                </td>
+                                <td>
+                                    @if($index > 0)
+                                        <a href="#" type="button"
+                                           class="btn btn-danger btn-sm btn-remove-package-element">Quitar</a>
+                                    @endif
+                                </td>
+                            </tr>
+                        @endforeach
+                    @else
                         <tr>
                             <td>
-                                {{ Form::text('description[' . $identifier . ']', $item->description, ['class' => 'form-control', 'placeholder' => 'Ingresa la descripción del elemento', 'maxlength' => 100, 'required' => 'required']) }}
+                                {{ Form::text('description[ab12c]', null, ['class' => 'form-control', 'placeholder' => 'Ingresa la descripción del elemento', 'maxlength' => 100, 'required' => 'required']) }}
                             </td>
                             <td>
-                                {{ Form::select('service_category_id[' . $identifier . ']', $serviceCategories, $item->service_category->id, ['class' => 'form-control', 'placeholder' => 'Escoja una categoría', 'required' => 'required']) }}
+                                {{ Form::select('service_category_id[ab12c]', $serviceCategories, null, ['class' => 'form-control', 'placeholder' => 'Escoja una categoría', 'required' => 'required']) }}
+                            </td>
+                            <td></td>
+                        </tr>
+                    @endif
+                @else
+                    <?php $count = 0; ?>
+                    @foreach($old['description'] as $identifier => $item)
+                        <tr>
+                            <td>
+                                {{ Form::text('description[' . $identifier . ']', $item, ['class' => 'form-control', 'placeholder' => 'Ingresa la descripción del elemento', 'maxlength' => 100, 'required' => 'required']) }}
                             </td>
                             <td>
-                                @if($index > 0)
-                                    <a href="#" type="button" class="btn btn-danger btn-sm btn-remove-package-element">Quitar</a>
+                                {{ Form::select('service_category_id[' . $identifier . ']', $serviceCategories, $old['service_category_id'][$identifier], ['class' => 'form-control', 'placeholder' => 'Escoja una categoría', 'required' => 'required']) }}
+                            </td>
+                            <td>
+                                @if($count > 0)
+                                    <a href="#" type="button"
+                                       class="btn btn-danger btn-sm btn-remove-package-element">Quitar</a>
                                 @endif
                             </td>
                         </tr>
+                        <?php $count++; ?>
                     @endforeach
-                @else
-                    <tr>
-                        <td>
-                            {{ Form::text('description[ab12c]', null, ['class' => 'form-control', 'placeholder' => 'Ingresa la descripción del elemento', 'maxlength' => 100, 'required' => 'required']) }}
-                        </td>
-                        <td>
-                            {{ Form::select('service_category_id[ab12c]', $serviceCategories, null, ['class' => 'form-control', 'placeholder' => 'Escoja una categoría', 'required' => 'required']) }}
-                        </td>
-                        <td></td>
-                    </tr>
                 @endif
+
 
                 </tbody>
             </table>
